08c4b2debdb1d80fdb8f1aebdd7b88df66c8fe05
Ian Namie <Iannamie82@gmail.com>
2023-05-18 11:43:33 -0500

    Finished exercise

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+namespace FizzBuzzChallenge
+{
+	public class Program
+	{
+		public static void Main()
+		{
+			FizzBuzz(0);
+			
+		}
+		public static void FizzBuzz(int num) 
+		{
+			Console.WriteLine("What number would you like to Check?");
+			num = int.Parse(Console.ReadLine());
+			if (num % 3 == 0 && num % 5 == 0) 
+			{
+                Console.WriteLine("FizzBuzz");
+            }
+			else if (num % 3 == 0)
+			{
+				Console.WriteLine("Fizz");
+			}
+			else if (num % 5 == 0) 
+			{
+				Console.WriteLine("buzz");
+			}
+			
+		
+		
+		}
+	}
+}
